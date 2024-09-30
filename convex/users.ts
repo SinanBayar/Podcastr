3904fe5d7248cf6339d11c9545c915b9a7b4380f
Sinan Bayar <sinanbayar88@gmail.com>
2024-09-30 22:00:14 +0300

    Convex setup and schema added

---
+++ convex/users.ts
@@ -0,0 +1,36 @@
+import { v } from "convex/values";
+import { internalMutation } from "./_generated/server";
+
+// export const createUser = internalMutation({
+//   args: {
+//     clerckId: v.string(),
+//     email: v.string(),
+//     imageUrl: v.string(),
+//     name: v.string(),
+//   },
+//   handler: async (ctx, args) => {
+//     await ctx.db.insert("users", {
+//       clerckId: args.clerckId,
+//       email: args.email,
+//       imageUrl: args.imageUrl,
+//       name: args.name,
+//     });
+//   },
+// });
+
+export const createUser = internalMutation({
+  args: {
+    clerckId: v.string(),
+    email: v.string(),
+    imageUrl: v.string(),
+    name: v.string(),
+  },
+  handler: async (ctx, { clerckId, email, imageUrl, name }) => {
+    await ctx.db.insert("users", {
+      clerckId,
+      email,
+      imageUrl,
+      name,
+    });
+  },
+});
