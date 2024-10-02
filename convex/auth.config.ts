ee011cf7bedb00a8f9330a609d0104759e92b878
Sinan Bayar <sinanbayar88@gmail.com>
2024-10-02 22:37:32 +0300

    Clerk entegrated with Convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://dynamic-parakeet-67.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
--- convex/users.ts
+++ convex/users.ts
@@ -1,36 +1,113 @@
-import { v } from "convex/values";
-import { internalMutation } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import { internalMutation, query } from "./_generated/server";
 
-// export const createUser = internalMutation({
-//   args: {
-//     clerckId: v.string(),
-//     email: v.string(),
-//     imageUrl: v.string(),
-//     name: v.string(),
-//   },
-//   handler: async (ctx, args) => {
-//     await ctx.db.insert("users", {
-//       clerckId: args.clerckId,
-//       email: args.email,
-//       imageUrl: args.imageUrl,
-//       name: args.name,
-//     });
-//   },
-// });
+export const getUserId = query({
+  args: { clerkId: v.string() },
+  handler: async (ctx, { clerkId }) => {
+    const user = await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("clerkId"), clerkId))
+      .unique();
+    if (!user) {
+      throw new ConvexError("User not found");
+    }
+    return user;
+  },
+});
+
+// this query is used to get the top user by podcast count. first the podcast is sorted by views and then the user is sorted by total podcasts, so the user with the most podcasts will be at the top.
+export const getTopUserByPodcastCount = query({
+  args: {},
+  handler: async (ctx, args) => {
+    const user = await ctx.db.query("users").collect();
+
+    const userData = await Promise.all(
+      user.map(async (u) => {
+        const podcasts = await ctx.db
+          .query("podcasts")
+          .filter((q) => q.eq(q.field("authorId"), u.clerkId))
+          .collect();
+
+        const sortedPodcasts = podcasts.sort((a, b) => b.views - a.views);
+
+        return {
+          ...u,
+          totalPodcasts: podcasts.length,
+          podcast: sortedPodcasts.map((p) => ({
+            podcastTitle: p.podcastTitle,
+            pocastId: p._id,
+          })),
+        };
+      })
+    );
+
+    return userData.sort((a, b) => b.totalPodcasts - a.totalPodcasts);
+  },
+});
 
 export const createUser = internalMutation({
   args: {
-    clerckId: v.string(),
+    clerkId: v.string(),
     email: v.string(),
     imageUrl: v.string(),
     name: v.string(),
   },
-  handler: async (ctx, { clerckId, email, imageUrl, name }) => {
+  handler: async (ctx, { clerkId, email, imageUrl, name }) => {
     await ctx.db.insert("users", {
-      clerckId,
+      clerkId,
       email,
       imageUrl,
       name,
     });
+  },
+});
+
+export const updateUser = internalMutation({
+  args: {
+    clerkId: v.string(),
+    email: v.string(),
+    imageUrl: v.string(),
+  },
+  handler: async (ctx, { clerkId, email, imageUrl }) => {
+    const user = await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("clerkId"), clerkId))
+      .unique();
+    if (!user) {
+      throw new ConvexError("User not found");
+    }
+    await ctx.db.patch(user._id, {
+      email,
+      imageUrl,
+    });
+
+    const podcast = await ctx.db
+      .query("podcasts")
+      .filter((q) => q.eq(q.field("authorId"), clerkId))
+      .collect();
+
+    await Promise.all(
+      podcast.map(async (p) => {
+        await ctx.db.patch(p._id, {
+          authorImageUrl: imageUrl,
+        });
+      })
+    );
+  },
+});
+
+export const deleteUser = internalMutation({
+  args: { clerkId: v.string() },
+  async handler(ctx, { clerkId }) {
+    const user = await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("clerkId"), clerkId))
+      .unique();
+
+    if (!user) {
+      throw new ConvexError("User not found");
+    }
+
+    await ctx.db.delete(user._id);
   },
 });
