b2955a94c481a7fc90632faf76346385142591a3
Sinan Bayar <sinanbayar88@gmail.com>
2024-11-19 17:40:42 +0300

    Added MobileNavfor mobile view

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,7 +1,78 @@
-const MobileNav = () => {
-  return (
-    <div>Mobile Nav</div>
-  )
-}
+"use client";
 
-export default MobileNav
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { sidebarLinks } from "@/constants";
+
+const MobileNav = () => {
+  const route = useRouter();
+  const pathname = usePathname();
+  return (
+    <section className="text-white-1">
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            alt="Menu"
+            width={30}
+            height={30}
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-black-1">
+          <Link
+            href="/"
+            className="flex cursor-pointer item-center gap-1 pb-10 pl-4"
+          >
+            <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+            <h1 className="text-24 font-extrabold text-white-1 ml-2">
+              Podcastr
+            </h1>
+          </Link>
+
+          <div className="flex flex-col justify-between overflow-y-auto h-[calc(100vh-72px)]">
+            <SheetClose asChild>
+              <nav className="flex flex-col gap-6 text-white-1">
+                {sidebarLinks.map(({ route, label, imgURL }) => {
+                  const isActive =
+                    pathname === route || pathname.startsWith(`${route}/`);
+                  return (
+                    <SheetClose key={route} asChild>
+                      <Link
+                        href={route}
+                        className={cn(
+                          "flex gap-3 items-center py-4 max-lg:px-4 justify-start",
+                          {
+                            "bg-nav-focus border-r-4 border-orange-1": isActive,
+                          }
+                        )}
+                      >
+                        <Image
+                          src={imgURL}
+                          alt={label}
+                          width={24}
+                          height={24}
+                        />
+                        <p>{label}</p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </nav>
+            </SheetClose>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
+
+export default MobileNav;
