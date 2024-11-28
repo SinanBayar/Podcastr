be6d414143f6a50601a71638d0b5598150558dc4
Sinan Bayar <sinanbayar88@gmail.com>
2024-11-29 01:06:20 +0300

    Added login and logout buttons on MobileNav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -10,16 +10,18 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
-import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { sidebarLinks } from "@/constants";
-import { useClerk } from "@clerk/nextjs";
+import { SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 const MobileNav = () => {
-  const route = useRouter();
+  const router = useRouter();
   const pathname = usePathname();
-  const { user } = useClerk();
+  const { user, signOut } = useClerk();
+
   return (
     <section className="text-white-1">
       <Sheet>
@@ -33,23 +35,27 @@ const MobileNav = () => {
           />
         </SheetTrigger>
         <SheetContent side="left" className="border-none bg-black-1">
-          <SheetHeader>
+          <SheetHeader className="hidden">
             <SheetTitle />
             <SheetDescription />
           </SheetHeader>
-          <Link
-            href="/"
-            className="flex cursor-pointer item-center gap-1 pb-10 pl-4"
-          >
-            <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
-            <h1 className="text-24 font-extrabold text-white-1 ml-2">
-              Podcastr
-            </h1>
-          </Link>
-
-          <div className="flex flex-col justify-between overflow-y-auto h-[calc(100vh-72px)]">
-            <SheetClose asChild>
-              <nav className="flex flex-col gap-6 text-white-1">
+          <SheetClose asChild>
+            <section className="mobile_nav">
+              <nav className="flex flex-col gap-6">
+                <Link
+                  href="/"
+                  className="flex cursor-pointer item-center gap-1 pb-10"
+                >
+                  <Image
+                    src="/icons/logo.svg"
+                    alt="logo"
+                    width={23}
+                    height={27}
+                  />
+                  <h1 className="text-24 font-extrabold text-white-1 ml-2">
+                    Podcastr
+                  </h1>
+                </Link>
                 {sidebarLinks.map(({ route, label, imgURL }) => {
                   const isActive =
                     pathname === route ||
@@ -62,7 +68,7 @@ const MobileNav = () => {
                           label == "My Profile" ? `${route}/${user?.id}` : route
                         }
                         className={cn(
-                          "flex gap-3 items-center py-4 max-lg:px-4 justify-start",
+                          "flex gap-3 items-center py-4 justify-start",
                           {
                             "bg-nav-focus border-r-4 border-orange-1": isActive,
                           }
@@ -80,8 +86,26 @@ const MobileNav = () => {
                   );
                 })}
               </nav>
-            </SheetClose>
-          </div>
+              <div className="flex-center w-full pb-14 pr-8 mt-10">
+                <SignedOut>
+                  <Button
+                    asChild
+                    className="text-16 w-full bg-orange-1 font-extrabold"
+                  >
+                    <Link href="/sign-in">Sign in</Link>
+                  </Button>
+                </SignedOut>
+                <SignedIn>
+                  <Button
+                    className="text-16 w-full bg-orange-1 font-extrabold"
+                    onClick={() => signOut(() => router.push("/"))}
+                  >
+                    Log out
+                  </Button>
+                </SignedIn>
+              </div>
+            </section>
+          </SheetClose>
         </SheetContent>
       </Sheet>
     </section>
